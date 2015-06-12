c05b9cb06f256bae4ac1b06963b092582c98e99a
AmyBeall <amylouisebeall@gmail.com>
2015-06-12 16:48:44 -0700

    added user auth

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser'); //get body-parser
 var morgan  = require('morgan'); //used to see requests
 var mongoose = require('mongoose'); //for working w/ our database
 var port	= process.env.PORT || 8080; //set the port for our app
+var jwt = require('jsonwebtoken');
+var superSecret = 'amysfirstwebtoken';
 
 // APP CONFIGURATION ---------------------
 // use body parser so we can grab information from POST requests
@@ -36,16 +38,95 @@ var apiRouter = express.Router();
 // test route to make sure everything is working
 // accessed at GET http://localhost:8080/api
 
+apiRouter.post('/authenticate', function(req,res){
+
+	//find the user
+	//select the name username and password explicitly
+	User.findOne({
+		username: req.body.username
+	}).select('name username password').exec(function(err,user){
+
+		if(err) throw err;
+
+		//no user with that username was found
+		if(!user){
+			res.json({
+				success: false,
+				message: 'Authentication failed. User not found.'
+			});
+		} else if (user) {
+
+			//check if password matches
+			var validPassword = user.comparePassword(req.body.password);
+			if(!validPassword){
+				res.json({
+					success: false,
+					message: 'Authentication failed. Wrong password.'
+				});
+			} else {
+
+				//if user is found and password is right
+				//create a token
+				var token = jwt.sign({
+					name: user.name,
+					username: user.username
+				}, superSecret, {
+					expiresInMinutes: 1440 //expires in 24 hours
+				});
+
+				//return the information including the token as JSON
+				res.json({
+					success: true,
+					message: 'Enjoy your token!',
+					token: token
+				});
+			}
+
+		}
+	});
+});
+
 //middleware to use for all requests
 apiRouter.use(function(req,res,next){
-	//do logging
-	console.log('Somebody just came to our app!');
-	//this is where we will authenticate users
-	next();
-	//make sure to use next so that our routes don't stop here!
+	
+	//check header or url parameters or post parameters for token
+	var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+	//decode token
+	if(token){
+
+		//verifies secret and checks exp
+		jwt.verify(token, superSecret, function(err, decoded){
+			if(err){
+				return res.status(403).send({
+					success: false,
+					message: 'Failed to authenticate token.'
+				});
+			} else {
+				//if everything is good, save to request for use in other routes
+				req.decoded = decoded;
+
+				next();
+			}
+		});
+	} else {
+
+		//if there is no token
+		//return an HTTP response of 403 (access forbidden) and an error message
+		return res.status(403).send({
+			success: false,
+			message: 'No token provided.'
+		});
+	}
+
 });
+
 apiRouter.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });
+});
+
+apiRouter.get('/me', function(req,res){
+	res.send(req.decoded);
 });
 
 apiRouter.route('/users')
