9864a60072df57be20e18c41b4fb5f75135c67d2
AmyBeall <amylouisebeall@gmail.com>
2015-06-30 11:49:55 -0700

    third commit

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 var express = require('express');
 var app 	= express();
-var bodyParser = require('body-parser');
-var morgan  = require('morgan');
 
 var path = require('path');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/public'));
 
